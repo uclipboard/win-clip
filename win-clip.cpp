f720d7a289a8425ba5ca78bcc438b6f9a494fdf4
dangjinghao <dangjinghaoemail@163.com>
2024-05-13 21:41:29 +0800

    typo update

--- win-clip.cpp
+++ win-clip.cpp
@@ -40,10 +40,10 @@ void help() {
 	exit(0);
 }
 
-std::wstring utf8_to_wstr(const std::string& utf8string) {
+std::wstring convert_UTF8_to_wstr(const std::string& utf8string) {
 	if (utf8string.empty()) return std::wstring();
 
-	int wideSize = MultiByteToWideChar(
+	int wc_size = MultiByteToWideChar(
 		CP_UTF8,
 		0,
 		utf8string.c_str(),
@@ -52,26 +52,45 @@ std::wstring utf8_to_wstr(const std::string& utf8string) {
 		0
 	);
 
-	if (wideSize == 0) {
+	if (wc_size == 0) {
 		std::cerr << "Failed to get wideSzie" << std::endl;
 		return std::wstring();
 	}
 
-	std::wstring wstr(wideSize, 0); // 预分配宽字符字符串  
+	std::wstring wstr(wc_size, 0);   
 	MultiByteToWideChar(
 		CP_UTF8,
 		0,
 		utf8string.c_str(),
 		-1,
-		&wstr[0],         // 输出缓冲区  
-		wideSize         // 宽度  
+		&wstr[0],        
+		wc_size          
 	);
 
 	return wstr;
 }
 
-#define FAIL() {CloseClipboard();return 1;}
 int copy_UTF8_to_clipboard(std::string& msg) {
+
+	auto wmsg = convert_UTF8_to_wstr(msg);
+	auto data = wmsg.c_str();
+	auto data_size = wcslen(data) + 1;
+
+	HGLOBAL hGlobal = GlobalAlloc(GMEM_MOVEABLE, data_size * sizeof(WCHAR));
+	if (!hGlobal) {
+		std::cerr << "Failed to allocate memory" << std::endl;
+		return 1;
+	}
+
+	LPWSTR pGlobal = static_cast<LPWSTR>(GlobalLock(hGlobal));
+	if (!pGlobal) {
+		std::cerr << "Failed to lock memory" << std::endl;
+		GlobalFree(hGlobal);
+		return 1;
+	}
+
+	wcscpy_s(pGlobal, data_size, data);
+	GlobalUnlock(hGlobal);
 
 	if (!OpenClipboard(nullptr)) {
 		std::cerr << "Failed to open clipboard" << std::endl;
@@ -79,31 +98,13 @@ int copy_UTF8_to_clipboard(std::string& msg) {
 	}
 
 	EmptyClipboard();
-	auto wmsg = utf8_to_wstr(msg);
-	auto data = wmsg.c_str();
-	size_t dataSize = wcslen(data) + 1;
-
-	HGLOBAL hGlobal = GlobalAlloc(GMEM_MOVEABLE, dataSize * sizeof(wchar_t));
-	if (!hGlobal) {
-		std::cerr << "Failed to allocate memory" << std::endl;
-		FAIL();
-	}
-
-	wchar_t* pGlobal = static_cast<wchar_t*>(GlobalLock(hGlobal));
-	if (!pGlobal) {
-		std::cerr << "Failed to lock memory" << std::endl;
-		GlobalFree(hGlobal);
-		FAIL();
-	}
-
-	wcscpy_s(pGlobal, dataSize, data);
-	GlobalUnlock(hGlobal);
 
 
 	if (SetClipboardData(CF_UNICODETEXT, hGlobal) == nullptr) {
 		std::cerr << "Failed to set clipboard data" << std::endl;
 		GlobalFree(hGlobal);
-		FAIL();
+		CloseClipboard();
+		return 1;
 	}
 
 	CloseClipboard();
@@ -111,42 +112,44 @@ int copy_UTF8_to_clipboard(std::string& msg) {
 
 }
 int copy_ANSI_to_clipboard(std::string& msg) {
-	if (!OpenClipboard(nullptr)) {
-		std::cerr << "Failed to open clipboard" << std::endl;
-		return 1;
-	}
 
-	EmptyClipboard();
 	auto data = msg.c_str();
-	size_t dataSize = strlen(data) + 1;
+	auto data_size = strlen(data) + 1;
 
-	HGLOBAL hGlobal = GlobalAlloc(GMEM_MOVEABLE, dataSize);
+	HGLOBAL hGlobal = GlobalAlloc(GMEM_MOVEABLE, data_size);
 	if (!hGlobal) {
 		std::cerr << "Failed to allocate memory" << std::endl;
-		FAIL();
+		return 1;
 	}
 
 	LPSTR pGlobal = static_cast<LPSTR>(GlobalLock(hGlobal));
 	if (!pGlobal) {
 		std::cerr << "Failed to lock memory" << std::endl;
 		GlobalFree(hGlobal);
-		FAIL();
+		return 1;
 	}
 
-	memcpy(pGlobal, data, dataSize);
+	memcpy(pGlobal, data, data_size);
 	GlobalUnlock(hGlobal);
 
+	if (!OpenClipboard(nullptr)) {
+		std::cerr << "Failed to open clipboard" << std::endl;
+		return 1;
+	}
+
+	EmptyClipboard();
 
 	if (SetClipboardData(CF_TEXT, hGlobal) == nullptr) {
 		std::cerr << "Failed to set clipboard data" << std::endl;
 		GlobalFree(hGlobal);
-		FAIL();
+		CloseClipboard();
+		return 1;
 	}
 
 	CloseClipboard();
 	return 0;
 }
-#undef FAIL
+
 
 
 int paste_ascii_from_clipboard(std::string& s) {
@@ -156,7 +159,7 @@ int paste_ascii_from_clipboard(std::string& s) {
 		return 1;
 	}
 	// Lock the clipboard data to get a pointer to it
-	char* cbText = static_cast<char*>(GlobalLock(hData));
+	LPSTR cbText = static_cast<LPSTR>(GlobalLock(hData));
 	if (cbText == nullptr) {
 		std::cerr << "Unable to lock the clipboard data." << std::endl;
 		return 1;
@@ -167,41 +170,35 @@ int paste_ascii_from_clipboard(std::string& s) {
 }
 
 std::string wstr_to_ANSI_str(std::wstring& wstr) {
-	std::string s{};
+
 	auto cs = wstr.c_str();
-	// Convert the wide character string to ANSI
-	char* mbText = nullptr;
-	int mbLen = WideCharToMultiByte(CP_ACP, 0, cs, -1, nullptr, 0, nullptr, nullptr);
-	if (mbLen > 0) {
-		mbText = new char[mbLen];
-		WideCharToMultiByte(CP_ACP, 0, cs, -1, mbText, mbLen, nullptr, nullptr);
-		s = mbText;
-		delete[] mbText;
+	
+	auto ANSI_str_len = WideCharToMultiByte(CP_ACP, 0, cs, -1, nullptr, 0, nullptr, nullptr);
+	std::string s(ANSI_str_len,0);
+	if (ANSI_str_len > 0) {
+		WideCharToMultiByte(CP_ACP, 0, cs, -1, &s[0], ANSI_str_len, nullptr, nullptr);
 
 	}
 	return s;
 }
 
 std::string wstr_to_UTF8_str(std::wstring& wstr) {
-	std::string s{};
+
 	auto cs = wstr.c_str();
-	// Convert the wide character string to UTF-8
-	char* mbText = nullptr;
-	int mbLen = WideCharToMultiByte(CP_UTF8, 0, cs, -1, nullptr, 0, nullptr, nullptr);
-	if (mbLen > 0) {
-		mbText = new char[mbLen];
-		WideCharToMultiByte(CP_UTF8, 0, cs, -1, mbText, mbLen, nullptr, nullptr);
-		s = mbText;
-		delete[] mbText;
+	auto UTF8_str_len = WideCharToMultiByte(CP_UTF8, 0, cs, -1, nullptr, 0, nullptr, nullptr);
+	std::string s(UTF8_str_len,0);
+	if (UTF8_str_len > 0) {
+		
+		WideCharToMultiByte(CP_UTF8, 0, cs, -1, &s[0], UTF8_str_len, nullptr, nullptr);
 	}
 	return s;
 }
 
 
 
-int paste_wchars_from_clipboard(HANDLE& hData, std::wstring& s) {
+int paste_wstring_from_clipboard(HANDLE& hData, std::wstring& s) {
 	// Lock the clipboard data to get a pointer to it
-	wchar_t* wcbText = static_cast<wchar_t*>(GlobalLock(hData));
+	LPWSTR wcbText = static_cast<LPWSTR>(GlobalLock(hData));
 	if (wcbText == nullptr) {
 		std::cerr << "Unable to lock the clipboard data." << std::endl;
 		return 1;
@@ -225,13 +222,12 @@ std::string paste_from_clipboard(bool isUTF8) {
 	HANDLE hData = GetClipboardData(CF_UNICODETEXT);
 	if (hData == nullptr) {
 		// If we can't get CF_UNICODETEXT, try getting CF_TEXT
-
 		paste_ascii_from_clipboard(s);
 	}
 	else {
 
 		std::wstring ws;
-		paste_wchars_from_clipboard(hData, ws);
+		paste_wstring_from_clipboard(hData, ws);
 		if (isUTF8) {
 			s = wstr_to_UTF8_str(ws);
 		}
@@ -248,20 +244,21 @@ std::string paste_from_clipboard(bool isUTF8) {
 
 
 void copy(std::string& arg_msg, bool isUTF8IN) {
-	std::string a_msg;
+	std::string clipboard_data;
 	if (arg_msg.empty()) {
+		// I don't know why should I use c++ style like that!!
 		std::string in_str{ (std::istreambuf_iterator<char>(std::cin)),std::istreambuf_iterator<char>() };
-		a_msg = in_str;
+		clipboard_data = in_str;
 	}
 	else {
-		a_msg = arg_msg;
+		clipboard_data = arg_msg;
 	}
 	int ret;
 	if (isUTF8IN) {
-		ret = copy_UTF8_to_clipboard(a_msg);
+		ret = copy_UTF8_to_clipboard(clipboard_data);
 	}
 	else {
-		ret = copy_ANSI_to_clipboard(a_msg);
+		ret = copy_ANSI_to_clipboard(clipboard_data);
 
 	}
 	exit(ret);
