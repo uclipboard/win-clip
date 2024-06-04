4fe46345debed3347a7e458749c41dd8f639d8ac
dangjinghao <dangjinghaoemail@163.com>
2024-06-04 19:44:48 +0800

    add windows application metadata

--- readme.md
+++ readme.md
@@ -6,3 +6,7 @@ This is a part of uclipboard project
 
 ## Bugs or Features
 win-clip supports `win-clip copy -m hello world` to copy `hello wolrd` to system clipboard. But in fact, it operate on an approach of concatenate text with whitespace as the separate . So If you want to copy some data contains no-whitespace invisible character, please use quotation. 
+
+Support watch mode: execute a command as soon as clipboard changed.
+
+See `win-clip.exe -h` for more details.
--- resource.h
+++ resource.h
@@ -2,10 +2,29 @@
 // Microsoft Visual C++ generated include file.
 // Used by win-clip.rc
 
+#pragma once
+
+#define MAJOR_VERSION 0
+#define MINOR_VERSION 1
+#define PATCH_VERSION 4
+
+#define STRINGIFY(x) #x
+#define VERSION_JOIN(x, y, z) STRINGIFY(x) "." STRINGIFY(y) "." STRINGIFY(z)
+
+#define WC_FILEVERSION 1, MAJOR_VERSION , MINOR_VERSION ,PATCH_VERSION
+#define WC_PRODUCTVERSION 1,MAJOR_VERSION ,MINOR_VERSION ,PATCH_VERSION
+#define WC_COMPANY_NAME "uclipboard"
+#define WC_FILE_DESCRIPTION "A part of uclipboard."
+#define WC_FILE_VERSION "1." VERSION_JOIN(MAJOR_VERSION,MINOR_VERSION,PATCH_VERSION)
+#define WC_INTERNAL_NAME "win-clip.exe"
+#define WC_LEGAL_COPYRIGHT "Copyright (C) 2024 uclipboard."
+#define WC_ORIGINAL_FILENAME "win-clip.exe"
+#define WC_PRODUCT_NAME "win-clip"
+#define WC_PRODUCT_VERSION VERSION_JOIN(MAJOR_VERSION,MINOR_VERSION,PATCH_VERSION)
 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        101
+#define _APS_NEXT_RESOURCE_VALUE        103
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
